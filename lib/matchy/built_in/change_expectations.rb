4e332119ddbbba50b475215336b51652ab6895d4
josh gruenberg <joshgr@gmail.com>
2009-07-20 22:04:59 -0700

    providing more meaningful messages for change_expectations

--- lib/matchy/built_in/change_expectations.rb
+++ lib/matchy/built_in/change_expectations.rb
@@ -11,18 +11,23 @@ module Matchy
       def change(&block)
         build_matcher(:change) do |receiver, matcher, args|
           before, done, after = block.call, receiver.call, block.call
-          comparison = after != before
-          if list = matcher.chained_messages
-            comparison = case list[0].name
-              # todo: provide meaningful messages
-            when :by          then (after == before + list[0].args[0] || after == before - list[0].args[0])
-            when :by_at_least then (after >= before + list[0].args[0] || after <= before - list[0].args[0])
-            when :by_at_most  then (after <= before + list[0].args[0] && after >= before - list[0].args[0])
-            when :from        then (before == list[0].args[0]) && (after == list[1].args[0])
+	  change_description = ''
+          comparison = if list = matcher.chained_messages
+	    message = list.first
+	    arg = message.args.first
+	    name = message.name
+	    change_description = " #{name.to_s.gsub('_', ' ')} #{arg.inspect}"
+            case name
+            when :by          then (after == before + arg || after == before - arg)
+            when :by_at_least then (after >= before + arg || after <= before - arg)
+            when :by_at_most  then (after <= before + arg && after >= before - arg)
+            when :from        then (before == arg) && (after == list[1].args[0])
             end
+	  else
+	    after != before
           end
-          matcher.positive_failure_message = "given block should alter the given value"
-          matcher.negative_failure_message = "given block shouldn't alter the given value"
+          matcher.positive_failure_message = "given block should alter the given value#{change_description};\n  was #{before.inspect},\n  now #{after.inspect}"
+	  matcher.negative_failure_message = "given block should not alter the given value#{change_description};\n  was: #{before.inspect},\n  now: #{after.inspect}"
           comparison
         end
       end

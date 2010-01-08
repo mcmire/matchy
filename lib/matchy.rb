0a381f5d67046c8455cd234025efefa80fad3876
Elliot Winkler <elliot.winkler@gmail.com>
2010-01-08 00:13:09 -0600

    Er, let's try that one more time

--- lib/matchy.rb
+++ lib/matchy.rb
@@ -1,6 +1,8 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'test/unit/assertions'
+
 require 'matchy/expectation_builder'
 require 'matchy/modals'
 require 'matchy/matcher_builder'
@@ -13,7 +15,7 @@ require 'matchy/built_in/truth_expectations'
 require 'matchy/built_in/operator_expectations'
 require 'matchy/built_in/change_expectations'
 
-if const_defined?(:Protest)
+if Object.const_defined?(:Protest)
   Protest::TestCase.send(:include, Matchy::Expectations::TestCaseExtensions)
 else
   require 'test/unit'

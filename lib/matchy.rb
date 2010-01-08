dc17dba97cc534787bf18102640afac7c7c5d0c0
Elliot Winkler <elliot.winkler@gmail.com>
2010-01-08 00:04:43 -0600

    Mix in Matchy to Protest if it is loaded, otherwise mix into Test::Unit

--- lib/matchy.rb
+++ lib/matchy.rb
@@ -1,8 +1,6 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-require 'test/unit'
-
 require 'matchy/expectation_builder'
 require 'matchy/modals'
 require 'matchy/matcher_builder'
@@ -15,5 +13,11 @@ require 'matchy/built_in/truth_expectations'
 require 'matchy/built_in/operator_expectations'
 require 'matchy/built_in/change_expectations'
 
-Test::Unit::TestCase.send(:include, Matchy::Expectations::TestCaseExtensions)
+if const_defined?(:Protest)
+  Protest::TestCase.send(:include, Matchy::Expectations::TestCaseExtensions)
+else
+  require 'test/unit'
+  Test::Unit::TestCase.send(:include, Matchy::Expectations::TestCaseExtensions)
+end
+
 include Matchy::CustomMatcher

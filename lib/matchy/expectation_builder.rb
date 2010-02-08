dc61f0dc40faa4edfa586e87b24b95184d85f964
Elliot Winkler <elliot.winkler@gmail.com>
2010-02-07 22:08:46 -0600

    Remove #pass! and #fail! methods from matcher classes so they actually match the RSpec API. This way hopefully we can use RSpec matchers along with Matchy w/o skipping a beat.
    Cherry-picked from technicalpickles' fork:
    * 9dfd0c160910fac6c14b5850491507a3db8c0e30
    * 6f77ba31765f86ed78ecb5c51a64e6ec5da1f075

--- lib/matchy/expectation_builder.rb
+++ lib/matchy/expectation_builder.rb
@@ -1,9 +1,13 @@
 module Matchy
   module ExpectationBuilder
-    def self.build_expectation(match, exp, obj)
-      return Matchy::Expectations::OperatorExpectation.new(obj, match) unless exp
+    def self.build_expectation(should_succeed, matcher, object)
+      return Matchy::Expectations::OperatorExpectation.new(object, should_succeed) unless matcher
       
-      (exp.matches?(obj) != match) ? exp.fail!(match) : exp.pass!(match)
+      if should_succeed
+        $current_test_case.assert_accepts(matcher, object)
+      else
+        $current_test_case.assert_rejects(matcher, object)
+      end
     end
   end
 end
